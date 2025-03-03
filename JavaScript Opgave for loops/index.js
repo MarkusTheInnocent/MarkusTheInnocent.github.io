70c14882ae109d1ef97337925ae33366aba2823d
MarkusTheMilfHunter <M.H.K.H.1945@gmail.com>
2025-03-03 13:33:17 +0100

    Nye opave begynder

---
+++ JavaScript Opgave for loops/index.js
@@ -0,0 +1,17 @@
+const products = [
+    {name: "Product 1", price: 10, image: "black-front.png", available: true},
+    {name: "Product 2", price: 15, image: "white-front.png", available: false},
+    {name: "Product 3", price: 20, image: "black-front.png", available: true},
+    {name: "Product 4", price: 10, image: "black-front.png", available: false},
+    {name: "Product 5", price: 5, image: "white-front.png", available: true},
+    {name: "Product 6", price: 30, image: "black-front.png", available: true},
+];
+
+
+
+
+for (const abe of products) {
+    console.log(abe)
+
+}
+
