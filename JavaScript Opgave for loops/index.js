045be0da43ea1cc58142163d39a88b8664e881d8
MarkusTheMilfHunter <M.H.K.H.1945@gmail.com>
2025-03-07 12:00:55 +0100

    Jeg kan nu lave produkt cards
    epic

--- JavaScript Opgave for loops/index.js
+++ JavaScript Opgave for loops/index.js
@@ -8,10 +8,43 @@ const products = [
 ];
 
 
+let showUnavailabeProducts = false
+
+let produktCardList = document.querySelector('#produktCard');
 
 
-for (const abe of products) {
-    console.log(abe)
+produktCardList.innerHTML = ""
 
+
+for (let produkt of products) {
+    let myList = ""
+    myList += "<section class='stylecool'>"
+    myList += "<p>"+ produkt.name +"</p>"
+    myList += "<img src='Images/"+ produkt.image +"'></img>"
+    myList += "<p>"+ produkt.price +" Kr.</p>"
+    myList += "<p>available: "+ produkt.available +"</p>"
+    myList += "</section>"
+    produktCardList.innerHTML += myList;
+
+   
 }
 
+function show(){
+
+    produktCardList.innerHTML = ""
+
+    if (showUnavailabeProducts == false && produkt.available == true || showUnavailabeProducts == true ) {
+        produktCardList.innerHTML += myList;
+
+    }
+}
+
+
+let filterButton = document.querySelector('#filter');
+
+ function hide() {
+    showUnavailabeProducts = true;
+    show()
+
+    }
+ 
