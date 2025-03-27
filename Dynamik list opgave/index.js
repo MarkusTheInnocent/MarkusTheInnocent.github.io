3d1efcb1aefafce9f5aea0bfe94c47258b6da1c3
MarkusTheMilfHunter <M.H.K.H.1945@gmail.com>
2025-03-27 10:45:22 +0100

    async funktioner

--- Dynamik list opgave/index.js
+++ Dynamik list opgave/index.js
@@ -1,39 +1,62 @@
 
 
-fetch('https://dummyjson.com/todos')
-.then(turnToJson)
-.then(getItem)
 
 
-function turnToJson(response) {
-    return response.json()
-}
 
-function getItem(items) {
-    console.log(items.todos)
-    let myList = document.querySelector('#listeDisplay')
-   
+
+
+
+async function fetchTodos() {
+    let response = await fetch('https://dummyjson.com/todos')
+    let jsonResponse = await response.json()
+
+    //vise todos - Tilføje dem til vores liste
+
+    let list = document.querySelector('#listeDisplay')
     
-for (let mytodo of items.todos) {
-    let todoText = mytodo.todo
-    let complete = mytodo.completed
-
-    let listStyle = ""
-    listStyle += '<li>'+ todoText +'</li>'
-    listStyle += '<li>'+ complete +'</li>'
-
-    myList.innerHTML += listStyle;
+    for (let todoItem of jsonResponse.todos) {
+        if(todoItem.completed) {
+            list.innerHTML += `<li>✅ ${todoItem.todo}</li>`
+        } else {
+            list.innerHTML += `<li>❌ ${todoItem.todo}</li>`
+        }
+    }
 }
 
+fetchTodos()
+
+
+let nextSkip = 30
+let totalt = 0
+
+async function susKnapClick() {
+
+    //Tilføj de næste 30 todos til vores API/liste
+
+
+    
+    if(totalt != 0 && nextSkip >= totalt){
+        return alert("Ikke mere")
+    }
+    
+    let response = await fetch(`https://dummyjson.com/todos?skip=${nextSkip}`)
+    let jsonResponse = await response.json()
+    totalt = jsonResponse.total
+
+    nextSkip += 30
+
+
+    //vise todos - Tilføje dem til vores liste
+
+    let list = document.querySelector('#listeDisplay')
+    
+    for (let todoItem of jsonResponse.todos) {
+        if(todoItem.completed) {
+            list.innerHTML += `<li>✅ ${todoItem.todo}</li>`
+        } else {
+            list.innerHTML += `<li>❌ ${todoItem.todo}</li>`
+        }
+    }
 }
 
 
-
-
-function susKnapClick() {
-     
-}
-
-fetch('https://dummyjson.com/todos?skip=30')
-.then(turnToJson)
-.then(getMoreThan)
